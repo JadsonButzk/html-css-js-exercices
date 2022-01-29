f89382d19ed8de4a2136e011f868a3e08f56b1fe
Jadson Butzk <84212592+JadsonButzk@users.noreply.github.com>
2022-01-29 13:58:21 -0300

    Add files via upload

---
+++ ex-01/script.js
@@ -0,0 +1,17 @@
+function carregar(){
+    var msg=document.getElementById('msg')
+    var img=document.getElementById('imagem')
+    var data= new Date()
+    var hora=data.getHours()
+    msg.innerHTML= `Agora são ${hora} horas.`
+    if (hora>=0 && hora<12){
+        img.src = 'manhã....png'
+        document.body.style.background='#fecb71'
+    }else if(hora>=12 && hora<18){
+        img.src = 'tarde....png'
+        document.body.style.background='#cac38c'
+    }else {
+        img.src = 'noite....png'
+        document.body.style.background='#197679'
+    }
+}
